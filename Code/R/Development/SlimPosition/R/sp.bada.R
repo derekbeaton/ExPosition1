43ba2cbd0e2816aa10e89fbec8c480e22bbb66a2
derek <you@example.com>
2017-07-16 21:33:09 -0400

    for JRR

--- Code/R/Development/SlimPosition/R/sp.bada.R
+++ Code/R/Development/SlimPosition/R/sp.bada.R
@@ -1,1 +1,3 @@
+## TODO: JRR
+
 #bada
--- Code/R/Development/SlimPosition/R/sp.pca.R
+++ Code/R/Development/SlimPosition/R/sp.pca.R
@@ -1,4 +1,4 @@
-## pca
+## TODO: JRR
 
   ## I had a 'compact = T' here but I'm not sure what the role of compact would be anymore.
 sp.pca <- function(DATA, center = T, scale = T, k = 0, graphs = F){
