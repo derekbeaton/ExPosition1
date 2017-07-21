a08ae09cec924aa30a61c84d53b2e2e770a2f5a8
derek <you@example.com>
2017-07-21 01:41:50 -0400

    We have CA and RRR running
    Mostly. RRR (and friends) need LV plots.

--- Code/R/Development/SlimPosition/R/sp.ca.R
+++ Code/R/Development/SlimPosition/R/sp.ca.R
@@ -1,20 +1,22 @@
 ## plain ca (symmetric vs. asymmetric allowed)
-
-  ## it's just CA, that's it.
+## it's just CA, that's it.
 sp.ca <- function(DATA, k = 0, compact = T, graphs = F){
 
-  wi <- rowSums(DATA)/sum(DATA)
-  wj <- colSums(DATA)/sum(DATA)
-  Ox <- DATA/sum(DATA)
+  sum.data <- sum(DATA)
+  wi <- rowSums(DATA)/sum.data
+  wj <- colSums(DATA)/sum.data
+  #Ox <- DATA/sum(DATA)
 
-  res <- gsvd( Ox - (wi %o% wj), 1/wi, 1/wj, k = k )
-
+  res <- gsvd( (DATA/sum.data) - (wi %o% wj), 1/wi, 1/wj, k = k )
 
   if(graphs){
-
+    sp.component_plot(res$fi)
+    sp.component_plot(res$fj)
+    sp.scree(res$d.orig^2)
   }
   if(compact){
-
+    res <- list(fi=res$fi, fj=res$fj, d.orig=res$d.orig, u=res$u, v=res$v)
   }
 
+  return(res)
 }
--- Code/R/Development/SlimPosition/R/sp.rrr.R
+++ Code/R/Development/SlimPosition/R/sp.rrr.R
@@ -8,13 +8,21 @@ sp.rrr <- function(X, Y, center.X = T, scale.X = T, center.Y = T, scale.Y = T, k
   res <- gsvd(
     t(X) %*% Y, crossprod(X),  k = k
   )
-  LX <- X %*% res$fi %*% diag(1/res$d)
-  LY <- Y %*% res$fj %*% diag(1/res$d)
+    ## can be more efficient with the d multiply.
+  res$lx <- X %*% res$fi %*% diag(1/res$d)
+  res$ly <- Y %*% res$fj %*% diag(1/res$d)
 
   if(graphs){
-
+    sp.component_plot(res$fi)
+    sp.component_plot(res$fj)
+    ## just make a separate "sp.latentvar_plot() specifically for latent variables
+    #sp.component_plot( cbind(res$lx[,1], res$ly[,1] ), main="Latent variable 1", xlab="LX 1", ylab="LY 1", asp=NA)
+    #sp.component_plot( cbind(res$lx[,2], res$ly[,2] ), main="Latent variable 2", xlab="LX 2", ylab="LY 2", asp=NA)
+    sp.scree(res$d.orig^2)
   }
   if(compact){
 
   }
+
+  return(res)
 }
