66c9910281b49d5d05cc7926cdf5e4292426f768
jennyrieck <jenny.rieck@gmail.com>
2017-08-11 16:18:21 -0400

    jr tests

--- Code/R/Development/SlimPosition/jr.test.R
+++ Code/R/Development/SlimPosition/jr.test.R
@@ -1,8 +1,8 @@
 setwd("C:/Users/Jenny/Documents/projects/ExPosition-Family/Code/R/Development/SlimPosition/")
 
-code.dir<-"../R/"
-code.fns<-list.files(code.dir,"R")
-sapply(code.fns,source)
+code.dir<-"./R/"
+code.fns<-list.files(path=code.dir,pattern="*.R$")
+sapply(paste0(code.dir,code.fns),source)
 
 load("../data/two.table.wine.rda")
 
@@ -11,9 +11,11 @@ pca.res<-sp.pca(wine$objective,center=T,scale=T,k=4,compact=T,graphs=T)
 bada.res<-sp.bada(DATA=wine$objective,center=T,scale=T,DESIGN = wine$supplemental$varietal,
                   make.data.nominal = T,k = 0,compact = T,graphs = T)
 
-load("../data/dica.wine.rda")
+load("./data/dica.wine.rda")
 
-dica.res<-sp.dica(DATA=dica.wine$data,make.data.nominal = T,
-                  DESIGN=dica.wine$design,
-                  k = 0,compact = T,graphs = make.data.nominal)
+library(ExPosition)
+
+dica.res<-sp.dica(DATA=dica.wine$data,make.data.nominal = F,
+                  DESIGN=rep(c("L","R","B"),each=4),
+                  k = 0,compact = T,graphs = T)
 
