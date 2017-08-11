6c2d7c6374cb8bf7633538167ab329bd494d848f
jennyrieck <jenny.rieck@gmail.com>
2017-08-11 14:58:34 -0400

    BADA PLS

--- Code/R/Development/SlimPosition/R/sp.bada.R
+++ Code/R/Development/SlimPosition/R/sp.bada.R
@@ -1,3 +1,36 @@
-## TODO: JRR
-
 #bada
+#make design nominal feature? User resonsible for making nominal?
+
+sp.bada <- function(DATA, center = T, scale = T, DESIGN, make.data.nominal = T, k = 0, compact = T, graphs = F){
+  
+  if(length(DESIGN)!=nrow(DATA)){
+    stop("DESIGN is not a vector of length nrow(DATA). A row design is required.")
+  }
+  DESIGN <- makeNominalData(as.matrix(DESIGN))
+  if( any(colSums(DESIGN)<2) ){
+    stop("A group size of 1 has been found. An observation cannot be the only observation in a group.")
+  }
+  
+  DESIGN.labs<-colnames(DESIGN)
+  DESIGN <- t(apply(DESIGN, 1, "/", colSums(DESIGN)))
+  colnames(DESIGN)<-DESIGN.labs
+  
+  res <- gsvd(expo.scale(t(DESIGN) %*% expo.scale(DATA, center = center, scale = scale),center=T,scale=F), k = k)
+  
+  res$lx <- X %*% res$fi * matrix(1/res$d,nrow(fi),ncol(fi),byrow=T)
+  res$ly <- DESIGN %*% res$fi * matrix(1/res$d,nrow(fi),ncol(fi),byrow=T)
+  
+  if(graphs){
+    sp.component_plot(res$fi)
+    sp.component_plot(res$fj)
+    sp.latentvar_plot(res)
+    sp.latentvar_plot(res,axis=2)
+    sp.scree(res$d.orig^2)
+    
+  }
+  if(compact){
+    res <- list(fi=res$fi, fj=res$fj, d.orig=res$d.orig, u=res$u, v=res$v, lx=res$lx)
+    
+  }
+  return(res)
+}
--- Code/R/Development/SlimPosition/R/sp.pls.R
+++ Code/R/Development/SlimPosition/R/sp.pls.R
@@ -1,3 +1,32 @@
 ## TODO: JRR
 
 ## pls
+
+sp.pls <- function(X, Y, center.X = T, scale.X = T, center.Y = T, scale.Y = T, k = 0, compact = T, graphs = F){
+  
+  if (nrow(X) != nrow(Y)) {
+    stop("X and Y must have the same number of rows.")
+  }
+  
+  X <- expo.scale(X, scale = scale.X, center = center.X)
+  Y <- expo.scale(Y, scale = scale.Y, center = center.Y)
+  
+  res<-gsvd(t(X) %*% Y, k=k)
+  
+  res$lx <- X %*% res$fi * matrix(1/res$d,nrow(fi),ncol(fi),byrow=T)
+  res$ly <- Y %*% res$fj * matrix(1/res$d,nrow(fj),ncol(fj),byrow=T)
+  
+  if(graphs){
+    sp.component_plot(res$fi)
+    sp.component_plot(res$fj)
+    sp.latentvar_plot(res)
+    sp.latentvar_plot(res,axis=2)
+    sp.scree(res$d.orig^2)
+  }
+  
+  if(compact){
+    res <- list(fi=res$fi, fj=res$fj, d.orig=res$d.orig, u=res$u, v=res$v, lx=res$lx, ly=res$ly)
+    
+  }
+  return(res)
+}
