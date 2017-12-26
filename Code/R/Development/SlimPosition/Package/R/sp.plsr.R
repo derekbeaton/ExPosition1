4ea33d6c276641b0b667f27a4332dbc63b2a726d
Derek <derekbeaton@users.noreply.github.com>
2017-12-25 19:37:22 -0500

    changes again to PLSR
    I think I'm getting to something I'd like to settle on for PLSR. This should be a big help conceptually.

--- Code/R/Development/SlimPosition/Package/R/sp.plsr.R
+++ Code/R/Development/SlimPosition/Package/R/sp.plsr.R
@@ -10,10 +10,10 @@ sp.plsr <- function(X, Y, center.X = T, scale.X = "SS1", center.Y = T, scale.Y =
 
       ## maybe I need a replacement for these to use a sweep or apply
         ## and maybe I can updated expo.scale to be a more minimal "class"
-  Xorig <- X <- expo.scale(X, scale = scale.X, center = center.X)
+  X.orig <- X <- expo.scale(X, scale = scale.X, center = center.X)
     X.center <- attributes(X)$`scaled:center`
     X.scale <- attributes(X)$`scaled:scale`
-  Yorig <- Y <- expo.scale(Y, scale = scale.Y, center = center.Y)
+  Y.orig <- Y <- expo.scale(Y, scale = scale.Y, center = center.Y)
     Y.center <- attributes(Y)$`scaled:center`
     Y.scale <- attributes(Y)$`scaled:scale`
 
@@ -34,10 +34,10 @@ sp.plsr <- function(X, Y, center.X = T, scale.X = "SS1", center.Y = T, scale.Y =
     k <- min(c(k,X.rank,Y.rank))
   }
 
-  pLX.mat <- u.mat <- matrix(NA,ncol(X),k)
+  pred.u.mat <- u.mat <- matrix(NA,ncol(X),k)
   v.mat <- matrix(NA,ncol(Y),k)
   t.mat <- LX.mat <- LY.mat <- matrix(NA,nrow(X),k)
-  r2.x <- r2.y <- delta.vec <- beta.vec <- rep(NA,k)
+  cross.trace <- r2.x <- r2.y <- delta.vec <- beta.vec <- rep(NA,k)
   if(!compact){
     X.resids <- X.hats <- array(NA,dim=c(nrow(X),ncol(X),k))
   }
@@ -46,38 +46,37 @@ sp.plsr <- function(X, Y, center.X = T, scale.X = "SS1", center.Y = T, scale.Y =
   for(i in 1:k){
 
     res <- gsvd(t(X) %*% Y, k=1)  ## we could get real weird here some day. Basically a "predictive" CCA/RRR or even weighted PLSR.
+      cross.trace[i] <- sum(res$d.orig^2)
     u.mat[,i] <- res$u
     v.mat[,i] <- res$v
     delta.vec[i] <- res$d
-    rm(res) ## save space if possible.
+    rm(res) ## save space maybe
 
     LX.mat[,i] <- X %*% as.matrix(u.mat[,i])
     LY.mat[,i] <- Y %*% as.matrix(v.mat[,i])
 
     ## what makes it PLSR, essentially.
     t.mat[,i] <- as.matrix(LX.mat[,i] / sqrt(sum(LX.mat[,i]^2)))
-    pLX.mat[,i] <- t(X) %*% t.mat[,i]
+    pred.u.mat[,i] <- t(X) %*% t.mat[,i]
     beta.vec[i] <- c(t(LY.mat[,i]) %*% t.mat[,i])
 
 
-      ##  there has to be a way to combine these...
-    X <- X-(t.mat[,i] %*% t(pLX.mat[,i]))
+    ## these are component-wise/iterative hats and resids...
+    X.rec <- t.mat[,i] %*% t(pred.u.mat[,i])
+    X <- X - X.rec
       X[which(abs(X) < tol)] <- 0
       r2.x[i] <- (X.trace-sum(X^2)) / X.trace
-    Y <- Y-t(beta.vec[i] * t(t.mat[,i] %*% t(v.mat[,i])))
-      Y[which(abs(Y) < tol)] <- 0
-      r2.y[i] <- (Y.trace-sum(Y^2)) / Y.trace
-
-    ## don't do all of these if compact==T
-    #### NOTE: This is doable for PLSC
-
-    Y.hats[,,i] <- ( ((t.mat[,1:i] * matrix(beta.vec[1:i],nrow(Y),i,byrow=T)) %*% t(v.mat[,1:i])) * matrix(Y.scale,nrow(Y),ncol(Y),byrow=T) + matrix(Y.center,nrow(Y),ncol(Y),byrow=T))
-    Y.resids[,,i] <- Yorig - Y.hats[,,i]
     if(!compact){
-      X.hats[,,i] <- ( (t.mat[,1:i] %*% t(pLX.mat[,1:i])) * matrix(X.scale,nrow(X),ncol(X),byrow=T) + matrix(X.center,nrow(X),ncol(X),byrow=T))
-      X.resids[,,i] <- Xorig - X.hats[,,i]
+      X.hats[,,i] <- X.rec * matrix(X.scale,nrow(X),ncol(X),byrow=T) + matrix(X.center,nrow(X),ncol(X),byrow=T)
+      X.resids[,,i] <- (X.orig * matrix(X.scale,nrow(X),ncol(X),byrow=T) + matrix(X.center,nrow(X),ncol(X),byrow=T)) - X.hats[,,i]
     }
 
+    Y.rec <- t(beta.vec[i] * t(t.mat[,i] %*% t(v.mat[,i])))
+    Y <- Y - Y.rec
+      Y[which(abs(Y) < tol)] <- 0
+      r2.y[i] <- (Y.trace-sum(Y^2)) / Y.trace
+    Y.hats[,,i] <- Y.rec * matrix(Y.scale,nrow(Y),ncol(Y),byrow=T) + matrix(Y.center,nrow(Y),ncol(Y),byrow=T)
+    Y.resids[,,i] <- (Y.orig * matrix(Y.scale,nrow(Y),ncol(Y),byrow=T) + matrix(Y.center,nrow(Y),ncol(Y),byrow=T)) - Y.hats[,,i]
 
 
     if( sum(Y)==0 ){
@@ -92,26 +91,31 @@ sp.plsr <- function(X, Y, center.X = T, scale.X = "SS1", center.Y = T, scale.Y =
     }
   }
 
-  Xrec <- t.mat %*% t(pLX.mat)
-  Xhat <- (Xrec * matrix(attributes(X)$`scaled:scale`,nrow(X),ncol(X),byrow=T) + matrix(attributes(X)$`scaled:center`,nrow(X),ncol(X),byrow=T))
-
-  Yrec <- (t.mat * matrix(beta.vec,nrow(Y),length(beta.vec),byrow=T)) %*% t(v.mat)
-  Yhat <- (Yrec * matrix(attributes(Y)$`scaled:scale`,nrow(Y),ncol(Y),byrow=T) + matrix(attributes(Y)$`scaled:center`,nrow(Y),ncol(Y),byrow=T))
-
-
+    ## this is do-able for PLSC
+  Y.rec <- (t.mat * matrix(beta.vec,nrow(Y),i,byrow=T)) %*% t(v.mat)
+    Y.rec[which(abs(Y.rec) < tol)] <- 0
+  Y.hat <-  Y.rec * matrix(Y.scale,nrow(Y),ncol(Y),byrow=T) + matrix(Y.center,nrow(Y),ncol(Y),byrow=T)
+    Y.hat[which(abs(Y.hat) < tol)] <- 0
+  Y.resid <- (Y.orig * matrix(Y.scale,nrow(Y),ncol(Y),byrow=T) + matrix(Y.center,nrow(Y),ncol(Y),byrow=T)) - Y.hat
+  if(!compact){
+    X.rec <- t.mat %*% t(pred.u.mat)
+      X.rec[which(abs(X.rec) < tol)] <- 0
+    X.hat <- X.rec * matrix(X.scale,nrow(X),ncol(X),byrow=T) + matrix(X.center,nrow(X),ncol(X),byrow=T)
+      X.hat[which(abs(X.hat) < tol)] <- 0
+    X.resid <- (X.orig * matrix(X.scale,nrow(X),ncol(X),byrow=T) + matrix(X.center,nrow(X),ncol(X),byrow=T)) - X.hat
+  }
 
   ## just to conform to how it is done in PLSC
   fi <-  u.mat * matrix(delta.vec,nrow(u.mat),ncol(u.mat),byrow=T)
   fj <-  v.mat * matrix(delta.vec,nrow(v.mat),ncol(v.mat),byrow=T)
 
   if(compact){
-    #res <- list(fi=res$fi, fj=res$fj, d.orig=res$d.orig, u=res$u, v=res$v, lx=res$lx, ly=res$ly)
-    res <- list(fi=fi, fj=fj, d=delta.vec, u=u.mat, v=v.mat, lx=LX.mat, ly=LY.mat)
+    res <- list(fi=fi, fj=fj, d=delta.vec, u=u.mat, v=v.mat, lx=LX.mat, ly=LY.mat, Y.hats=Y.hats, Y.hat=Y.hat, Y.resid=Y.resid, r2.x=r2.x, r2.y=r2.y)
   }else{
-    res <- list(fi=fi, fj=fj, d=delta.vec, u=u.mat, v=v.mat, lx=LX.mat, ly=LY.mat, beta=beta.vec, t=t.mat, pLX=pLX.mat, Xhat=Xhat, Yhat=Yhat, X.hats=X.hats, Y.hats=Y.hats, X.resids=X.resids, Y.resids=Y.resids, r2.x=r2.x, r2.y=r2.y)
+    res <- list(fi=fi, fj=fj, d=delta.vec, cross.trace=cross.trace, u=u.mat, v=v.mat, lx=LX.mat, ly=LY.mat, beta=beta.vec, t=t.mat, pred.u=pred.u.mat, X.hats=X.hats, X.hat=X.hat, Y.hats=Y.hats, Y.hat=Y.hat, X.resids=X.resids, X.resid=X.resid, Y.resids=Y.resids, Y.resid=Y.resid, r2.x=r2.x, r2.y=r2.y)
   }
 
-  ## what is the common variance?
+  ## this will take some thinkin'
 
   if(graphs){
     #sp.component_plot(res$fi)
