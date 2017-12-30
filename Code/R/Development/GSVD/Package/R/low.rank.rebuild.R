d954ddc0871cdbde7036d8d57713b9c08871ea31
Derek <derekbeaton@users.noreply.github.com>
2017-12-30 14:58:34 -0500

    update to low rank rebuild

--- Code/R/Development/GSVD/Package/R/low.rank.rebuild.R
+++ Code/R/Development/GSVD/Package/R/low.rank.rebuild.R
@@ -1,59 +1,76 @@
 #'
 #'  @export
 #'
-#'  @title \code{low.rank.rebuild}: raise matrix to a power and rebuild lower rank version
+#'  @title \code{low.k}: raise matrix to a power and rebuild lower rank version
 #'
-#'  @description \code{low.rank.rebuild} takes in a matrix and will rebuild a lower rank estimate.
+#'  @description \code{low.k} takes in a matrix and will rebuild a lower rank estimate.
 #'
 #'  @param x data matrix
 #'  @param k the number of components to retain in order to build a lower rank estimate of \code{x}
 #'  @param ... parameters to pass through to \code{\link{tolerance.svd}}
 #'
 #'  @return
-#'  The (possibly lower rank) raised to an arbitrary \code{power} version of \code{x}
+#'  Low rank version of \code{x}
 #'
-#'  @seealso \code{\link{tolerance.svd}} and \code{\link{invert.rebuild_matrix}}
+#'  @seealso \code{\link{tolerance.svd}}, \code{\link{power.rebuild_matrix}}, and \code{\link{invert.rebuild_matrix}}
 #'
 #'  @examples
-#'  hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
-#'  X <- hilbert(9)[, 1:6]
-#'  X.power_1 <- low.rank.rebuild(X)
-#'  X / X.power_1
 #'
-#'  ## other examples.
-#'  X.power_2 <- low.rank.rebuild(X,power=2)
-#'  X.power_negative.1.div.2 <- low.rank.rebuild(X,power=-1/2)
-#'
-#'  X.power_negative.1 <- low.rank.rebuild(X,power=-1)
-#'  X.power_negative.1 / t(invert.rebuild_matrix(X))
 #'
 #'  @author Derek Beaton
 #'
 #'  @keywords multivariate, diagonalization, eigen, pseudo-inverse, Moore-Penrose
 #'
 
-low.rank.rebuild <- function(x, k=0, ...){
+
+low.k <- function(x, k = 0, ...){
+
+  ## quick tests for escape
+  if( !is.numeric(k) ){
+    stop("k is not numeric")
+  }else{
+    if( is.infinite(k) | is.nan(k) ){
+      stop("k is Inf or NaN")
+    }
+  }
+
+  if( length(k)==0 ){
+    stop("k is of length 0")
+  }
+
+  ##stolen from MASS::ginv()
+  if (length(dim(x)) > 2L || !(is.numeric(x) || is.complex(x)))
+    stop("low.rank.rebuild: 'x' must be a numeric or complex matrix")
+  if (!is.matrix(x))
+    x <- as.matrix(x)
 
 
-  # ##stolen from MASS::ginv()
-  # if (length(dim(x)) > 2L || !(is.numeric(x) || is.complex(x)))
-  #   stop("low.rank.rebuild: 'x' must be a numeric or complex matrix")
-  # if (!is.matrix(x))
-  #   x <- as.matrix(x)
-  #
-  # if(k<=0){
-  #   k <- min(nrow(x),ncol(x))
-  # }
-  #
-  # ## should be tested for speed.
-  #
-  # #res <- tolerance.svd(x,...)
-  # #comp.ret <- 1:min(length(res$d),k)
-  # #return( (res$u[,comp.ret] * matrix(res$d[comp.ret],nrow(res$u[,comp.ret]),ncol(res$u[,comp.ret]),byrow=T)) %*% t(res$v[,comp.ret]) )
-  #
-  # res <- tolerance.svd(x, nu = k, nv = k, ...)
-  # return( sweep(res$u,2,res$d[1:k],"*") %*% t(res$v) )
+  if( length(k) > 1 ){
 
-  return( power.rebuild_matrix(X,power=1,k=k,...) )
+    ## this allows for a rebuild of arbitrary components...
+    comps.to.rebuild <- unique( round(ifelse(k < 1, 1, k)) )
+    res <- tolerance.svd(x, nu = max(comps.to.rebuild), nv = max(comps.to.rebuild), ...)
+    return( sweep(res$u[,comps.to.rebuild],2,res$d[comps.to.rebuild],"*") %*% t(res$v[,comps.to.rebuild]) )
+
+
+  }else { #if(length(k) == 1){
+    if( rank.build <= 0 ){
+      stop("k is a negative or 0 value")
+    }
+
+    ## this allows for a rebuild of some cumulative percentage
+    if(k > 0 & k < 1 ){
+      res <- tolerance.svd(x)
+      comps.to.rebuild <- 1:(max(which( cumsum(res$tau) <= k))+1)
+      return( sweep(res$u[,comps.to.rebuild],2,res$d[comps.to.rebuild],"*") %*% t(res$v[,comps.to.rebuild]) )
+
+    ## this rebuilds based on first K components where K = round(k)
+    }else {#if( (k > 0) ){
+      res <- tolerance.svd(x, nu = round(k), nv = round(k) )
+      return( sweep(res$u,2,res$d[1:round(k)],"*") %*% t(res$v) )
+
+    }
+  }
+
 
 }
