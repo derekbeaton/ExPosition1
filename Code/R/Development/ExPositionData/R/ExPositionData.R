18bd3cfbd51f728c4ab54a1c36386c29ef615533
Derek <derekbeaton@users.noreply.github.com>
2018-02-10 16:56:24 -0500

    ExPo package is almost ready

---
+++ Code/R/Development/ExPositionData/R/ExPositionData.R
@@ -0,0 +1,5 @@
+#' ExPositionData
+#'
+#' The purpose of this package is to house all of the data sets that have been curated for use with ExPosition. All packages in the ExPosition family (besides this one) will have only limited and necessary data sets. This has many data sets for use and exploration.
+#'
+"_PACKAGE"
