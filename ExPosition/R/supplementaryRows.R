a44b45062312b0ba1e0a8ca1370840499b8a4a8d
Derek <piratedrock@gmail.com>
2022-03-25 13:14:55 -0400

    sort of a hail mary approach to fixing the && vs & thing with vectors of various lengths

--- ExPosition/R/supplementaryRows.R
+++ ExPosition/R/supplementaryRows.R
@@ -14,7 +14,7 @@ supplementaryRows <- function(SUP.DATA,res){
 	#TEST THIS FURTHER... I SHOULD BE ABLE TO RECOGNZIE TEHSE...	
 	if(class(res)[1] %in% output.types){
 		indicator <- which(output.types %in% class(res)[1])
-		if(names(res) %in% data.types && length(names(res))==2){
+		if(any(names(res) %in% data.types) && length(names(res))==2){
 			if(output.types[indicator]=="expoOutput"){
 				res <- res$ExPosition.Data
 			}
